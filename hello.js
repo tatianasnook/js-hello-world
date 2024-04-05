ccf5d61c0645e0991de7fa8d15f7096a7d7d18a0
tatianasnook <tatsnook@gmail.com>
2024-04-05 14:10:54 -0500

    first program

--- hello.js
+++ hello.js
@@ -4,7 +4,11 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log('Hello, World!');
+console.log(12 + 11)
+console.log('Hello, everyone!'); console.log('Hello, everyone!')
+console.log('She said: "Nevermore"'); 
+
 
 /*  
 Next, PLAY! 
